ebbb3765b2095b6afbc4f2b83efc54c50ee9579a
John <gromozeker@gmail.com>
2018-09-28 00:42:21 +0300

    revert done

---
+++ src/com/learning/strings/revert/Revert.java
@@ -0,0 +1,25 @@
+package com.learning.strings.revert;
+
+class Revert {
+    public static String revertSentence(String str) throws IllegalArgumentException {
+        if( str == null) {
+            throw new IllegalArgumentException("where is the string?");
+        }
+        StringBuilder temp = new StringBuilder();
+        StringBuilder result = new StringBuilder();
+
+        String modificatedString = temp.append(str).append(" ").toString();
+        temp.delete(0, temp.length());
+
+        for (int i = 0; i < modificatedString.length(); i++) {
+            temp.append(modificatedString.charAt(i));
+            if(modificatedString.charAt(i) == ' ' || i == modificatedString.length() - 1) {
+                result.append(temp.reverse());
+                temp.delete(0, temp.length());
+                continue;
+            }
+
+        }
+        return result.toString().trim();
+    }
+}
