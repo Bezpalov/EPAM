1f0c169e8cf2e3fde0a9096373ddaadc1b77c6a3
John <gromozeker@gmail.com>
2018-10-18 21:44:49 +0300

    have finished task

--- src/main/com/learning/recursion/Executable.java
+++ src/main/com/learning/recursion/Executable.java
@@ -1,16 +1,35 @@
 package main.com.learning.recursion;
 
 public interface Executable {
+
     StringBuilder execute();
 
+    /**
+     * Utility method that return pseudo random number in range
+     * from 0 to 25
+     *
+     * @return long primitive type from 0 to 25
+     */
     static Long getRandomPositiveNumber() {
         return (long) (Math.round(Math.random() * 25));
     }
 
+    /**
+     * Utility method that return pseudo random number in range
+     * from -25 to 25
+     *
+     * @return long primitive type from -25 to 25
+     */
     static Long getRandomNumber() {
         return -25 + Math.round(Math.random() * 50);
     }
 
+    /**
+     * Utility method that return pseudo random number in range
+     * from -25 to 0
+     *
+     * @return long primitive type from -25 to 0
+     */
     static Long getRandomNegativeNumber() {
         return (long) (-Math.round(Math.random() * 25));
     }
