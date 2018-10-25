d21be8dfc42f7a4e38747e915149c0316cf9cec5
Eugeniy Zozulya <gromozeker@gmail.com>
2018-10-25 17:43:01 +0300

    temporary state of launch logic

--- src/main/com/learning/bank/abilities/balance/ExistBalance.java
+++ src/main/com/learning/bank/abilities/balance/ExistBalance.java
@@ -1,11 +1,10 @@
-package main.com.learning.bank.balance;
+package main.com.learning.bank.abilities.balance;
 
 import main.com.learning.bank.Account;
 
 public class ExistBalance implements Balance {
     @Override
     public void showBalance(Account acc) {
-        System.out.print("Your balance is: ");
-        acc.showBalance();
+        System.out.println("Your balance is: " + acc.showBalance());
     }
 }
